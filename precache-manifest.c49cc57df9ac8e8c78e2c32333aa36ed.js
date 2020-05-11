cd33811d49c6dee1e986efd17c229921bef02a1c
andbilous-digis <andbilous@gmail.com>
2020-05-11 20:14:49 +0300

    Updates

---
+++ precache-manifest.c49cc57df9ac8e8c78e2c32333aa36ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837c10a818caab3d0d80854574e83f13",
+    "url": "/movies-task/index.html"
+  },
+  {
+    "revision": "588822b4c690433fc045",
+    "url": "/movies-task/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "d6be4258893227f60e84",
+    "url": "/movies-task/static/css/main.1a5f11bd.chunk.css"
+  },
+  {
+    "revision": "588822b4c690433fc045",
+    "url": "/movies-task/static/js/2.d14bffb5.chunk.js"
+  },
+  {
+    "revision": "d6be4258893227f60e84",
+    "url": "/movies-task/static/js/main.bd737167.chunk.js"
+  },
+  {
+    "revision": "f77522ea4c220708e681",
+    "url": "/movies-task/static/js/runtime~main.13278c35.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/movies-task/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
